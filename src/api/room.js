ad55d4aca9e2de575f3310ed7ccac37229f7dac4
lile <285051173@qq.com>
2018-01-01 15:38:35 +0800

    跨域cookie

--- src/api/room.js
+++ src/api/room.js
@@ -18,12 +18,6 @@ let RoomtApi = {
       data: condition
     });
   },
-  findCheckIn: function(id) {
-    return fetch({
-      url: this.fullPage('findCheckIn/' + id),
-      method: 'get'
-    });
-  },
   enable: function(id) {
     return fetch({
       url: this.fullPage('enable/' + id),
